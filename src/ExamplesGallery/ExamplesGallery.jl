9b4c1fcee7b32583fa99170e08ccf72b626d9279
Miguel Biron-Lattes <miguelbl9@gmail.com>
2022-08-09 15:21:33 -0700

    smol

--- src/ExamplesGallery/ExamplesGallery.jl
+++ src/ExamplesGallery/ExamplesGallery.jl
@@ -25,6 +25,13 @@ include("Testing/mvNormals.jl") # example with multivariate Normals admitting cl
 export MvNormalTM, free_energy, get_scaled_V_dist
 
 # define Turing models at startup so they are available when called inside dispatch
+# +info: an issue arises when running julia in HPC environments, where the cache
+# of precompiled stuff must be disable to avoid race conditions between multiple
+# processes. But no having cache causes problems with @generated functions in
+# packages used by DynamicPPL. Particularly, we get a "The applicable method may be too new:"
+# error for function "is_propertynames_overloaded" in pkg ConstructionBase. See  
+# https://docs.julialang.org/en/v1/manual/methods/#Redefining-Methods
+using ConstructionBase
 function __init__()
     include(pkgdir(NRSTExp, "src", "ExamplesGallery", "Turing", "hierarchical_model_dppl.jl"))
 end
