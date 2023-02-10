845456f08d32cdc3b3d9c0092fb2cbbc3104c382
Miguel Biron-Lattes <miguelbl9@gmail.com>
2023-02-10 09:16:20 -0800

    new saveres

--- src/hyperparams.jl
+++ src/hyperparams.jl
@@ -9,19 +9,16 @@ function hyperparams(ns::NRSTSampler, rng::AbstractRNG, TE::AbstractFloat, Λ::A
     # NRST
     res   = parallel_run(ns, rng, TE=TE)
     ξ, _  = fit_gpd(res)                 # compute tail index of the distribution of number of visits to the top
-    
-    # get stats
     tlens = tourlengths(res)
     nvevs = NRST.get_nvevals.(res.trvec)
     TE    = last(res.toureff)
-    saveres!(df, "NRST", tlens, nvevs, TE, NRST.get_ntours(res))
+    nvtop = res.visits[end,1]+res.visits[end,2]
+    saveres!(df, "NRST", tlens, nvevs, TE, NRST.get_ntours(res), ξ, nvtop)
 
     # add other metadata
     N     = ns.np.N
-    nvtop = res.visits[end,1]+res.visits[end,2]
     insertcols!(df,
-        :N => N, :Lambda => Λ, :xi => ξ, :n_vis_top => nvtop,
-        :sum_nexpls => sum(ns.np.nexpls)
+        :N => N, :Lambda => Λ, :sum_nexpls => sum(ns.np.nexpls)
     )
 
     return df
