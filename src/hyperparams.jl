422cff6643619562d067bac371f5ec36ddef4ca5
Miguel Biron-Lattes <miguelbl9@gmail.com>
2023-02-10 09:43:20 -0800

    common interface for benchmark and hyperparams

--- src/hyperparams.jl
+++ src/hyperparams.jl
@@ -4,22 +4,10 @@
 ###############################################################################
 
 function hyperparams(ns::NRSTSampler, rng::AbstractRNG, TE::AbstractFloat, Λ::AbstractFloat)
-    df = DataFrame() # init empty DataFrame
-
-    # NRST
-    res   = parallel_run(ns, rng, TE=TE)
-    ξ, _  = fit_gpd(res)                 # compute tail index of the distribution of number of visits to the top
-    tlens = tourlengths(res)
-    nvevs = NRST.get_nvevals.(res.trvec)
-    TE    = last(res.toureff)
-    nvtop = res.visits[end,1]+res.visits[end,2]
-    saveres!(df, "NRST", tlens, nvevs, TE, NRST.get_ntours(res), ξ, nvtop)
-
-    # add other metadata
-    N     = ns.np.N
-    insertcols!(df,
-        :N => N, :Lambda => Λ, :sum_nexpls => sum(ns.np.nexpls)
+    df = DataFrame()                              # init empty DataFrame
+    benchmark_sampler!(ns,rng,df,id="NRST",TE=TE) # NRST
+    insertcols!(df,                               # add extra metadata
+        :N => ns.np.N, :Lambda => Λ, :sum_nexpls => sum(ns.np.nexpls)
     )
-
     return df
 end
--- src/utils.jl
+++ src/utils.jl
@@ -4,13 +4,37 @@
 
 get_TE(vNs::AbstractVector) = (sum(vNs) ^ 2) / (length(vNs)*sum(abs2, vNs))
 
+# 
+function benchmark_sampler!(
+    st::NRST.AbstractSTSampler,
+    rng::AbstractRNG,
+    df::AbstractDataFrame;
+    id::AbstractString,
+    TE::AbstractFloat = NaN,
+    ntours_small::Int = -1,
+    α::AbstractFloat  = NRST.DEFAULT_α,
+    δ::AbstractFloat  = NRST.DEFAULT_δ
+    )
+    if isnan(TE)
+        TE = last(parallel_run(st, rng, ntours=ntours_small).toureff)
+    end
+    ntours= NRST.min_ntours_TE(TE,α,δ)
+    res   = parallel_run(st, rng, ntours=ntours)
+    ξ, _  = fit_gpd(res)                           # compute tail index of the distribution of number of visits to the top
+    tlens = tourlengths(res)
+    nvevs = NRST.get_nvevals.(res.trvec)
+    TE    = last(res.toureff)                      # get better estimate
+    nvtop = res.visits[end,1]+res.visits[end,2]
+    saveres!(df, id, tlens, nvevs, TE, ntours, ξ, nvtop)
+end
+
 # store results into df
-function saveres!(df::AbstractDataFrame, proc, tlens, nvevs, TE, ntours)
+function saveres!(df::AbstractDataFrame, proc, tlens, nvevs, TE, ntours, ξ, nvtop)
     append!(df,
         DataFrame(
             proc=proc, rtser=sum(tlens), rtpar=maximum(tlens),
             costser=sum(nvevs), costpar=maximum(nvevs), TE=TE,
-            ntours=ntours
+            ntours=ntours, xi=ξ, n_vis_top=nvtop
         )
     )
 end
