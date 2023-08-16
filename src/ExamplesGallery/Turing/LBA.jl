16a47e45d082c86d875c1c90f61283b05845f68c
Miguel Biron-Lattes <miguelbl9@gmail.com>
2023-08-16 09:09:37 -0700

    use same data in LBA

--- src/ExamplesGallery/Turing/LBA.jl
+++ src/ExamplesGallery/Turing/LBA.jl
@@ -1,3 +1,4 @@
+# Source: https://github.com/Julia-Tempering/Pigeons.jl/issues/106
 # Specify LBA model
 @model function model_lba(data; min_rt=minimum(data.rt))
     # Priors
@@ -11,8 +12,11 @@
 end
 
 function LBAModel(;seed=45461)
+    Random.seed!(seed)
+
     # Generate some data with known parameters
     dist = LBA(ν=[3.0, 2.0], A=0.8, k=0.2, τ=0.3)
-    data = rand(MersenneTwister(seed), dist, 100)
+    data = rand(dist, 100)
+
     NRST.TuringTemperedModel(model_lba(data))
 end
